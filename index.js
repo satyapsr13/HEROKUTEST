1e33ba282ffbcae4dce6479f8a63b1551253e60d
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-11-29 08:05:03 -0500

    dh

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000||process.env.PORT
+const port = process.env.PORT  
 
 app.get('/', (req, res) => res.send('Hello Wodsfdfgsgrld!'))
+
+console.log("------------------------------");
+console.log(process.env.PORT);
+console.log("------------------------------");
+// console.log(process.env.PORT);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-
-
-
-
-
- 
