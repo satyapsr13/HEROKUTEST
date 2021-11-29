1f2f12a6c5dba22493c1ccf3397d5ccbbb746311
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-11-29 08:31:33 -0500

    sdfd

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||1000
+const port = process.env.PORT || 1000
 const data = require("./data.json")
 app.get('/', (req, res) => {
-    res.json(JSON.stringify(data));
+    res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(body),
+        'Content-Type': 'json'
+    });
+
+    res.json(data);
     console.log(data);
 
-})
+
+});
+app.get('/letsPlay', (req, res) => {
+    res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(body),
+        'Content-Type': 'json'
+    });
+    res.json({
+        "lets": "play",
+        "lesdfdts": "play",
+        "lesdts": "play",
+        "lesdfdts": "play",
+        "lesddts": "play",
+        "ledts": "play"
+    });
+    console.log(data);
+
+});
 app.use(express.json());
 
 console.log("------------------------------");
