3cc30659758910e5861d6ea6fd54340b9a65e896
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-11-29 08:14:47 -0500

    fsd

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express()
 const port = process.env.PORT||1000
 const data = require("./data.json")
 app.get('/', (req, res) => {
-    res.json(data);
+    res.json(JSON.stringify(data));
     console.log(data);
 
 })
+app.use(express.json());
 
 console.log("------------------------------");
 console.log(process.env.PORT);
